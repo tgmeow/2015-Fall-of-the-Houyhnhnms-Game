60bc76c933b0d0e133ac2245c2781b296ebf9a64
Tiger Mou <tigermou16@gmail.com>
2016-09-28 22:09:02 -0500

    testUpdate1

--- src/main/FallOfTheHouyhnhnms.java
+++ src/main/FallOfTheHouyhnhnms.java
@@ -21,6 +21,7 @@ public class FallOfTheHouyhnhnms extends PApplet {
 	private ControllersMenu settingsMenu;
 	private PopNodeManager nodeManager;
 	private PopAIController aiController;
+	private PopAIController aiController0;
 	private PopAIController aiController1;
 	private AnimSVGMinion tempMinion;
 	private AnimSVGMinion tempMinion2;
